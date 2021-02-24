5e13204c977e8b21d67fb2d12fbf3505bb65158f
JadsonLucena <jadsonlucena@gmail.com>
2021-02-24 15:58:37 -0300

    feat (upgrade): switching protocols
    Opening Handshake: https://tools.ietf.org/html/rfc6455#page-6

--- src/WebSocket.js
+++ src/WebSocket.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const crypto = require('crypto');
 
 class WebSocket extends EventEmitter {
 
@@ -54,7 +55,10 @@ class WebSocket extends EventEmitter {
                 socket.destroy();
 
             } else {
-            
+
+                socket.write(`HTTP/1.1 101 Switching Protocols\r\nUpgrade: WebSocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: ${crypto.createHash('sha1').update(request.headers['sec-websocket-key'].trim() +'258EAFA5-E914-47DA-95CA-C5AB0DC85B11').digest('base64')}\r\n\r\n`);
+                socket.setTimeout(0);
+
             }
 
         });
